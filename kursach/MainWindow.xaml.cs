ec34f0241f955d26622efcee1ea81d3dfaaa16cb
Acer <Acer@LAPTOP-NSNU3CD4>
2024-03-25 21:13:15 +0300

    Добавьте файлы проекта.

---
+++ kursach/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using kursach.View;
+
+namespace kursach
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Authorization_Click(object sender, RoutedEventArgs e)
+        {
+            WindowAuthorization wAut = new WindowAuthorization();
+            wAut.Show();
+        }
+
+        private void Button_Registration_Click(object sender, RoutedEventArgs e)
+        {
+            WindowRegistration wReg = new WindowRegistration();
+            wReg.Show();
+        }
+    }
+}
